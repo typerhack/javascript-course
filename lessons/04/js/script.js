305e4e729518ae869b390d19409d9103d53b0e6a
Daniel <daniel.dolatabadi@gmail.com>
2022-03-30 23:41:00 +0430

    lesson 4 done

--- lessons/04/js/script.js
+++ lessons/04/js/script.js
@@ -1,27 +1,34 @@
 // This is the openweather.com API Key
-let API = 'a63e47f37a743be15083ba6bebc0d0ef';
+let API = "a63e47f37a743be15083ba6bebc0d0ef";
 
 //The function to get the data
 getWeatherData = () => {
     let city = getCityName();
 
     // https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
-    const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API}?`
-    console.log(URL);
+    const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API}&units=metric`;
+
+    // console.log(URL);
+
 
     fetch(URL)
-        .then(response => console.log(response.json()))
-        .then(data => console.log(data));
+        .then(response => response.json())
+        .then((data) => showResult(data))
+        .catch(error => console.log(error));
 
-    showResult(city);
 
-}
+
+};
 
 // Get the city name
 function getCityName() {
-    return document.querySelector('#city-input').value;
+    return document.querySelector("#city-input").value;
 }
 
-function showResult(city) {
-    document.querySelector('#card-title').value = city;
+function showResult(data) {
+    document.querySelector("#city-nam-output").innerText = data.name;
+    document.querySelector("#card-title").innerText = data.weather[0].description;
+    document.querySelector("#temp").innerText = data.main["temp"];
+    document.querySelector("#mintemp").innerText = data.main['temp_min'];
+    document.querySelector("#maxtemp").innerText = data.main["temp_max"];
 }
